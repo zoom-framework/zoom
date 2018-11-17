f83ea914e8e60b19a709414375e70a311fa6d30b
JZoom <jzoom8112@gmail.com>
2018-11-17 18:40:21 +0800

    Update install.sh

--- .boot/database/oracle/install.sh
+++ .boot/database/oracle/install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# working dir
-WORKSPACE=$(cd `dirname $0`; pwd)
-
-docker pull wnameless/oracle-xe-11g
+docker build -t oracle/test .
 
 # startup oracle
 docker run -p 1521:1521 \
-	-v ${WORKSPACE}/oracle.sh:/home/auto_service_bash.sh \
-	-t -i wnameless/oracle-xe-11g /home/auto_service_bash.sh
+	-d -i oracle/test /usr/local/work/oracle.sh
 
