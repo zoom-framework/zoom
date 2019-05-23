176aa55c7375c84d80477f81ea3dc7fffe420950
JZoom <jzoom8112@gmail.com>
2019-05-24 01:11:07 +0800

    Update StreamClassLoader.java

--- zoom-common/src/main/java/org/zoomdev/zoom/common/utils/StreamClassLoader.java
+++ zoom-common/src/main/java/org/zoomdev/zoom/common/utils/StreamClassLoader.java
@@ -1,9 +1,11 @@
 package org.zoomdev.zoom.common.utils;
 
 import java.io.InputStream;
+import java.net.URL;
 
 public interface StreamClassLoader {
 
     InputStream getStream(String className);
 
+    URL getUrl(String className);
 }
